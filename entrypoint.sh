3d2e2800c226b7e3dde53dde8e22deaeabc39535
Satcom <47340064+Satcomx00-x00@users.noreply.github.com>
2024-03-21 13:20:45 +0000

    modified:   entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 # Run FDS simulation
+
+echo "Setting unlimited stack size..."
 ulimit -s unlimited
+
+echo "Setting unlimited virtual memory..."
 ulimit -v unlimited
+
+echo "Running FDS simulation..."
 mpiexec -n $MPI fds $FDS_FILE_PATH
 
 filename=$(basename "$FDS_FILE_PATH")
+echo "Filename extracted: $filename"
 
 filename_without_extension="${filename%.fds}"
-
+echo "Filename without extension: $filename_without_extension"
 
 ZIP_OUTPUT_FOLDER="$filename_without_extension-output.zip"
+echo "Output zip folder name: $ZIP_OUTPUT_FOLDER"
 
 echo "Zipping to $ZIP_OUTPUT_FOLDER ..."
-
 zip -r "/workdir/$ZIP_OUTPUT_FOLDER" "/workdir/$ZIP_ARCHIVE_INPUT" 2>&1 || {
     echo "Failed to zip $ZIP_ARCHIVE_INPUT. Exiting."
     exit 1
 }
 
-echo "zipped"
+echo "Zipping completed successfully. Output: $ZIP_OUTPUT_FOLDER"
